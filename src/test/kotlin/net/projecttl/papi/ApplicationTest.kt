b3e2ef8992c52bc599dea54a737484a0ee53b2f0
Project_IO <me@projecttl.net>
2023-12-11 16:09:37 +0900

    fix: remove .env file

--- src/test/kotlin/net/projecttl/papi/ApplicationTest.kt
+++ src/test/kotlin/net/projecttl/papi/ApplicationTest.kt
@@ -1,9 +1,13 @@
 package net.projecttl.papi
 
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
+import net.projecttl.papi.plugins.configureHTTP
 import net.projecttl.papi.plugins.configureRouting
 import kotlin.test.*
 
@@ -12,6 +16,7 @@ class ApplicationTest {
     fun testRoot() = testApplication {
         application {
             configureRouting()
+            configureHTTP()
         }
         client.get("/v3").apply {
             assertEquals(HttpStatusCode.OK, status)
